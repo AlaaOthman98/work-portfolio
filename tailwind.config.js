c2068b67d888a8cd16ca6f06551f437dda346dac
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-03 17:51:05 +0200

    enable group-hover variant in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "group-hover"],
+  },
   plugins: [],
-}
+};
