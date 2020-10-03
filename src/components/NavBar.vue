f26570ec487ddf3bc12f4f4303e5c0569608151e
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-03 17:52:21 +0200

    add header component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,13 @@
 <template>
-  <header>
-    <span class="text-green-500">STRV</span>
-    <h3>Our Work</h3>
-    <div>
-      <span></span>
-      <span></span>
-      <span></span>
+  <header class="flex justify-between items-center p-10">
+    <router-link :to="{ name: 'Home' }">
+      <h1 class="text-red-600 font-semibold text-5xl">STRV</h1>
+    </router-link>
+    <h3 class="hidden sm:block font-bold text-2xl uppercase">Our Work</h3>
+    <div class="group cursor-pointer">
+      <span class="block w-8 h-1 group-hover:bg-red-600 bg-black mb-1"></span>
+      <span class="block w-8 h-1 group-hover:bg-red-600 bg-black mb-1"></span>
+      <span class="block w-8 h-1 group-hover:bg-red-600 bg-black mb-1"></span>
     </div>
   </header>
 </template>
