1f2909bd0a690f530ebc05118c60dc3210811fa8
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-03 13:40:47 +0200

    inject and test TailwindCSS with VueJs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
+    <NavBar />
     <router-view />
   </div>
 </template>
 
-<style></style>
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <span class="text-green-500">STRV</span>
+    <h3>Our Work</h3>
+    <div>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
