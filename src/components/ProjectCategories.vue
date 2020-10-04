62940e0f989ff579e3838423cc9ef729de2cd58c
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-04 17:50:30 +0200

    edit mobile view for project categories navigation

--- src/components/ProjectCategories.vue
+++ src/components/ProjectCategories.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="px-10">
     <div>
-      <div class="sm:hidden cursor-pointer flex items-center">
+      <div class="md:hidden mb-10 cursor-pointer flex items-center">
         <span class="uppercase text-2xl text-gray-600 mr-1">Show:</span>
         <span class="uppercase text-2xl mr-1">All</span>
         <span class="uppercase text-sm mr-1">▼</span>
       </div>
-      <ul class="hidden sm:flex items-center justify-center my-16">
+      <ul class="hidden md:flex items-center justify-center my-16">
         <li
           v-for="cat in projectCategories"
           :key="cat"
