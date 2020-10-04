8ccbf0c04288546aaf58919d093ab124009c4a1e
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-04 13:40:37 +0200

    add MenuIcon component to NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,14 +4,17 @@
       <h1 class="text-red-600 font-semibold text-5xl">STRV</h1>
     </router-link>
     <h3 class="hidden sm:block font-bold text-2xl uppercase">Our Work</h3>
-    <div class="group cursor-pointer">
-      <span class="block w-8 h-1 group-hover:bg-red-600 bg-black mb-1"></span>
-      <span class="block w-8 h-1 group-hover:bg-red-600 bg-black mb-1"></span>
-      <span class="block w-8 h-1 group-hover:bg-red-600 bg-black mb-1"></span>
-    </div>
+
+    <MenuIcon />
   </header>
 </template>
 
 <script>
-export default {};
+import MenuIcon from "@/components/MenuIcon.vue";
+
+export default {
+  components: {
+    MenuIcon,
+  },
+};
 </script>
