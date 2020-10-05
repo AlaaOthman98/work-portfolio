32ade088e403bbff691f0201d787a3ba96861f45
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-05 03:06:27 +0200

    add FontAwesome package for icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/css/tailwind.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
