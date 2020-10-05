33381420c20d677ed44d455cd34a4f7bc70bfafc
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-05 11:58:12 +0200

    add brand package in font awesome

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import store from "./store";
 import "./assets/css/tailwind.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(fas);
+library.add(fab, fas);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
