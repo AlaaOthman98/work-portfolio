c6211f20e21afb8bb467a660b812089e998bf83c
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-05 02:36:50 +0200

    edit project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,12 +2,12 @@
   <div class="w-full cursor-pointer relative">
     <img :src="imagePath" alt="" />
     <h3
-      class="absolute top-0 px-10 py-6 text-white text-5xl md:text-4xl lg:text-5xl uppercase"
+      class="absolute top-0 px-10 py-6 text-white text-5xl md:text-4xl md:px-4 md:py-4 lg:text-5xl uppercase"
     >
       {{ cardInfo.title }}
     </h3>
     <span
-      class="absolute bottom-0 px-10 py-6 text-white text-xl md:text-sm lg:text-xl font-sans"
+      class="absolute bottom-0 px-6 py-6 text-white text-xl md:text-sm md:px-4 md:py-4 lg:text-xl font-sans whitespace-no-wrap"
       >{{ cardInfo.description }}</span
     >
   </div>
