39addb76f34afb6908d12ccec8372e71c9d63a6c
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-06 16:48:00 +0200

    separate capitalize logic from template

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,7 +3,7 @@
     <div>
       <div class="md:hidden mb-10 cursor-pointer flex items-center">
         <span class="uppercase text-2xl text-gray-600 mr-1">Show:</span>
-        <span class="uppercase text-2xl mr-1">{{ navList[0] }}</span>
+        <span class="uppercase text-2xl mr-1">{{ currentCategory }}</span>
         <span class="uppercase text-sm mr-1">▼</span>
       </div>
       <ul class="hidden md:flex items-center justify-center py-16">
@@ -12,10 +12,7 @@
           :key="li"
           class="uppercase whitespace-no-wrap tracking-wider p-2 mx-4 text-xl hover:text-primary-red cursor-pointer"
         >
-          <router-link
-            :to="{ name: li.charAt(0).toUpperCase() + li.slice(1) }"
-            >{{ li }}</router-link
-          >
+          <router-link :to="{ name: capitalized(li) }">{{ li }}</router-link>
         </li>
       </ul>
     </div>
@@ -27,6 +24,16 @@ export default {
   props: {
     navList: {
       type: Array,
+    },
+  },
+  methods: {
+    capitalized(li) {
+      return li.charAt(0).toUpperCase() + li.slice(1);
+    },
+  },
+  computed: {
+    currentCategory() {
+      return this.$route.name;
     },
   },
 };
