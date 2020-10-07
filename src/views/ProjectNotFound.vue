9bec41f30263a54f1d80527391832079701382aa
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-07 03:44:52 +0200

    enhance ProjectNotFound component design

--- src/views/ProjectNotFound.vue
+++ src/views/ProjectNotFound.vue
@@ -1,9 +1,21 @@
 <template>
-  <h1 class="text-3xl flex items-center justify-center p-24">
-    This project is not found.
+  <h1
+    class="text-2xl md:text-4xl flex items-center justify-center p-24 tracking-wide"
+  >
+    This project
+    <span class="mx-3 text-primary-red">
+      {{ routePath }}
+    </span>
+    is not found.
   </h1>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    routePath() {
+      return this.$route.path.slice(1);
+    },
+  },
+};
 </script>
