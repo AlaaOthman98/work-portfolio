c28a66d9597f9adb6869290b8cdfa9268a286ae0
Alaa Othman <alaa.othman4707@gmail.com>
2020-10-09 00:19:32 +0200

    use Vuex for tracking the switch of NavModal component

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    modalIsOpen: false,
+    modalNavItems: {},
+  },
+  mutations: {
+    SET_MODAL_STATUS(state) {
+      state.modalIsOpen = !state.modalIsOpen;
+    },
+    SET_MODAL_NAV_ITEMS(state, navItems) {
+      state.modalNavItems = navItems;
+    },
+  },
+  actions: {
+    toggleModal({ commit }, { modalStatus, modalItems }) {
+      commit("SET_MODAL_STATUS", modalStatus);
+      commit("SET_MODAL_NAV_ITEMS", modalItems);
+    },
+  },
   modules: {},
 });
